181613324ded6bba6d855d96a2cb303f26cffa94
Wouter Schaap <wschaap91@gmail.com>
2024-11-20 14:40:00 +0100

    Add gap in between workshop items

--- src/components/WorkshopList.vue
+++ src/components/WorkshopList.vue
@@ -1,28 +1,33 @@
 <script setup lang="ts">
-import WorkshopItem from './WorkshopItem.vue';
+    import WorkshopItem from './WorkshopItem.vue';
 
-defineProps<{
-    items: {
-        id: string,
-        name: string,
-        tickets: ({
-            price: string
-        } & Record<string, any>)[],
-        dates: ({
-            ['flat_date']: string
-        } & Record<string, any>)[],
-        shopUrl: string | undefined
-        // banner: string
-    }[]
-}>()
+    defineProps<{
+        items: {
+            id: string,
+            name: string,
+            tickets: ({
+                price: string
+            } & Record<string, any>)[],
+            dates: ({
+                ['flat_date']: string
+            } & Record<string, any>)[],
+            shopUrl: string | undefined
+            // banner: string
+        }[]
+    }>()
 
 </script>
 
 
 <template>
     <div class="workshops">
-        <WorkshopItem v-for="event in items" :key="event.id"
-                      :name="event.name" :price="event.tickets[0].price" :id="event.id" :url="event.shopUrl" :dates="event.dates"></WorkshopItem>
+        <WorkshopItem v-for="event in items"
+                      :key="event.id"
+                      :name="event.name"
+                      :price="event.tickets[0].price"
+                      :id="event.id"
+                      :url="event.shopUrl"
+                      :dates="event.dates"></WorkshopItem>
     </div>
 </template>
 
@@ -30,6 +35,7 @@ defineProps<{
 .workshops {
     width: 100%;
     display: grid;
+    gap: 2rem;
     grid-template-columns: repeat(4, 1fr);
 
     @container page-wrapper (width < 1500px) {
